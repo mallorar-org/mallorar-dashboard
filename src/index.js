2e2700f8adac05310827ffd47daf8e3ff8b7f9b6
Harmony <harmochiky2@gmail.com>
2021-09-02 11:34:10 +0200

    Test push (no changes)

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
-
 import "normalize.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-quill/dist/quill.snow.css";
 import "./assets/css/main.css";
 import "normalize.css";
-
+// test line
 import DashBoard from "./containers/DashContainer";
 
 const MallDashBoard = (
